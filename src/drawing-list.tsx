b099d7d57302f440f493c83e12ba44102573f133
Thanh Dang <thanh.dang@bounteous.com>
2022-08-04 13:11:46 -0500

    Added rules and qualifers to nonideal state

--- src/drawing-list.tsx
+++ src/drawing-list.tsx
@@ -8,6 +8,7 @@ import { EligibleChartsList } from "./eligible-charts-list";
 import { Callout, NonIdealState } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import logo from "./assets/bite5.png";
+import { Link } from "@blueprintjs/icons/lib/esm/generated/16px/paths";
 
 const renderDrawing = (drawing: Drawing) => (
   <DrawnSet key={drawing.id} drawing={drawing} />
@@ -30,9 +31,21 @@ export function DrawingList() {
           icon={<img src={logo} height={256} />}
           title="The Beast in the East 5 SMX Card Draw"
           action={
+            <>
             <Callout intent="danger" icon={IconNames.ArrowTopLeft}>
               Choose a tournament round, then click 'Draw'
             </Callout>
+            <a href="https://bit.ly/BITE5-SMX" target="_blank">
+            <Callout intent="primary" icon={IconNames.DOCUMENT}>
+              See tournament rules
+            </Callout>
+            </a>
+            <a href="https://docs.google.com/spreadsheets/d/1ZVVcTKbEEj13qaLj8CfT9q3doOFMe6RZWuRYHfjdf1A/" target="_blank">
+            <Callout intent="success" icon={IconNames.LIST_COLUMNS}>
+              See current qualifier standings
+            </Callout>
+            </a>
+            </>
           }
         />
       </div>
